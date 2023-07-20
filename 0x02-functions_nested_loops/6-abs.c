a4632f8c1ed0990dc7aef541739c951d6d3263ef
meshackpupa <meshackpupa@gmail.com>
2023-07-20 13:57:44 -0700

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+
+/**
+ * the absolute value of an integer
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
